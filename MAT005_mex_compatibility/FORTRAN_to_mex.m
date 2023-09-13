637c2d9d86c798bfbcbe187dacbad79737532307
Daniel Knapp <daniel.y.knapp@gmail.com>
2023-09-13 09:36:49 +0200

    GCC compilers replaced by Intel alternatives for Windows.
    Former-commit-id: d21948e77fae4b932bb8a74fc0e100f5579f103e

--- MAT005_mex_compatibility/FORTRAN_to_mex.m
+++ MAT005_mex_compatibility/FORTRAN_to_mex.m
@@ -16,12 +16,13 @@ if isunix       % For unix-based
     % Delete unneeded files
     !rm trajectory_integration.mod trajectory_integration_module.o
 elseif ispc     % For windows
-    % Compile FORTRAN
-    !gfortran -Wall -Ofast -fPIC -c -cpp FOR001_modules/trajectory_integration_module.f
     % Generate header file
-    !gfortran -fc-prototypes -fsyntax-only -cpp FOR001_modules/trajectory_integration_module.f > C__001_wrappers/trajectory_integration_module.h
+    !gfortran -fc-prototypes -fsyntax-only -cpp FOR001_modules\trajectory_integration_module.f > C__001_wrappers\trajectory_integration_module.h
+    % Compile to .o file
+    !"C:\Program Files (x86)\Intel\oneAPI\setvars.bat" intel64 vs2022
+    !ifort /fpp /c /dll /O3 /Qopenmp /Qparallel .\FOR001_modules\trajectory_integration_module.f -o .\FOR001_modules\tim.o
     % Compile C wrapper to mex
-    mex -r2018a C__001_wrappers/trajectory_integration_module.c trajectory_integration_module.o -outdir FOR001_modules/
-    % Delete unneeded files
-    !del trajectory_integration.mod trajectory_integration_module.o
+    mex -r2018a C__001_wrappers/trajectory_integration_module.c FOR001_modules/tim.o -outdir FOR001_modules
+    % Remove extra files
+    !del FOR001_modules\tim.o
 end
